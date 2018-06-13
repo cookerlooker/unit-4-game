89a69108c4634d5049154410b75bae2356a04501
Rochelle Schofield <CodeGirl@macusers-MBP.attlocal.net>
2018-06-12 23:48:57 -0400

    Fist Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,83 @@
+// Institute Variable //
 
+    var targetNumber  = "";    
+    var wins = 0;
+    var losses = 0;
+    var counter = 0;
+    var images = ["./assets/images/Crystal-Blue.png", "./assets/images/Crystal-Purple.png", "./assets/images/Crystal-Yellow.png", "./assets/images/Crystal-Red.png"]
+    
+// Functions //
+
+        function randomTargetNumber () {
+            targetNumber = Math.floor(Math.random() * 120) + 19;
+        }
+    
+       
+
+
+        function resetCrystals () {
+            for (var i = 0; i < images.length; i++) {
+                var crystal = $(".crystal-images");
+                crystal.append("<img src='" + images[i] + "' alt='crystal'>");
+                crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+                crystal.attr("height", "100");
+                $(".crystal.images").append(crystal);
+            
+            
+            }
+        }
+        
+
+        function resetHtml () {
+            $(".target-number").html(targetNumber);
+            $(".win-lose-counter").html("<p>Wins: " + wins + "<p>Losses: " + losses + "</p>");
+            $(".score-number").html(counter);
+            $(".crystal-images").empty();
+        }
+
+        function totalReset () {
+            randomTargetNumber ();
+            counter = 0;
+            resetHtml ();
+            resetCrystals ();
+        }
+    
+// Running Code //
+
+        // Initial Page Set Up
+        randomTargetNumber();
+        resetHtml ();
+        resetCrystals ();
+
+// Click Functions //  
+
+        function crystalClick () {
+            // This attr returns first value of selected html element
+            counter += parseInt($(this).attr("value"));
+            $(".score-number").html(counter);
+            if (counter == targetNumber) {
+                wins++;
+                totalReset();
+            }
+            else if (counter > targetNumber) {
+                losses++;
+                totalReset();
+            };
+        };
+
+ // For Lifecycle of document, every single time document is dynamically changed execute crystalClick function // 
+ 
+ $(document).on("click", ".crystal", crystalClick);
+
+
+
+
+
+
+
+
+
+
+
+
+    
